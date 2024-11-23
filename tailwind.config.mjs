a48193ba681713414a49df32a690b4150e8ebd8d
Osián <149536063+OsianJL@users.noreply.github.com>
2024-11-23 08:41:51 +0000

    initial commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'accent': 'rgb(136, 58, 234)',
+				'accent-light': 'rgb(224, 204, 250)',
+        		'accent-dark': 'rgb(49, 10, 101)',
+			},
+			backgroundImage: {
+				'accent-gradient': 'linear-gradient(45deg, rgb(136, 58, 234), rgb(224, 204, 250) 30%, white 60%)',
+			}
+		},
 	},
 	plugins: [],
 }
