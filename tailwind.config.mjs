29b6d3566c30e76499fd85fcedc8db407d2d2a08
Osián <149536063+OsianJL@users.noreply.github.com>
2024-11-23 09:55:10 +0000

    janemillares base

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,14 +4,26 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'accent': 'rgb(136, 58, 234)',
+				'accent': 'rgb(174, 125, 230)',
 				'accent-light': 'rgb(224, 204, 250)',
         		'accent-dark': 'rgb(49, 10, 101)',
 			},
 			backgroundImage: {
 				'accent-gradient': 'linear-gradient(45deg, rgb(136, 58, 234), rgb(224, 204, 250) 30%, white 60%)',
+			},
+			fontFamily: {
+				sans: ['system-ui', 'sans-serif']
 			}
 		},
 	},
-	plugins: [],
+	// corePlugins: {
+	// 	preflight: false, 
+	//   },
+	plugins: [
+		function ({ addBase }) {
+		  addBase({
+			'*': { boxSizing: 'border-box' },
+		  });
+		},
+	  ],
 }
